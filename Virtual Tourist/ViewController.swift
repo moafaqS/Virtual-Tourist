20ba1645f728040a15c48ff5f4ee249f8652dda5
moafaq <imoafaqsimbawa@gmail.com>
2019-10-14 08:11:53 +0300

    Initial Commit

---
+++ Virtual Tourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Virtual Tourist
+//
+//  Created by moafaq waleed simbawa on 15/02/1441 AH.
+//  Copyright © 1441 moafaq. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
